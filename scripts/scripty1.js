9c0b3f629ab0c3c8dc3b608c612ced18909f0b6b
stephanista <houser16@uw.edu>
2017-07-15 14:24:13 -0700

    Pulls out the console logs checking the vars in addTask

--- scripts/scripty1.js
+++ scripts/scripty1.js
@@ -8,11 +8,8 @@ tasks = [
  function addTask() {
    event.preventDefault();
    var newtaskname = document.userTaskAdd.taskName.value;
-   console.log(newtaskname);
    var newtaskmaster = document.querySelector('input[name="taskymaster"]:checked').value;
-   console.log(newtaskmaster);
    var newtasklevel = document.userTaskAdd.diff.value;
-   console.log(newtasklevel);
    newtaskfull = [newtaskname, newtaskmaster, newtasklevel]
    tasks.push(newtaskfull);
    console.log("Oh, you added.. '" + newtaskfull + "'. Awesome!")
