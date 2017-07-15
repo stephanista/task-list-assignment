adbfa0e13a9b61dd36dfd871fa77f118a10d89b9
stephanista <houser16@uw.edu>
2017-07-14 20:25:18 -0700

    Cleans up code a bit, and adds console logs to figure out what variable is even breaking

--- scripts/scripty1.js
+++ scripts/scripty1.js
@@ -1,1 +1,19 @@
  console.log("heya");
+
+tasks = [
+
+]
+
+
+ function addTask() {
+   event.preventDefault();
+   var newtaskname = document.userTaskAdd.taskName.value;
+   console.log(newtaskname);
+   var newtaskmaster = document.querySelector('input[name="taskymaster"]:checked').value;
+   console.log(newtaskmaster);
+   var newtasklevel = document.userTaskAdd.taskLevel.value;
+   console.log(newtasklevel);
+   newtaskful = [newtaskname, newtaskmaster, newtasklevel]
+   tasks.push(newtaskfull);
+   console.log("Oh, you added.. '" + newtaskfull + "'. Awesome!")
+ }
